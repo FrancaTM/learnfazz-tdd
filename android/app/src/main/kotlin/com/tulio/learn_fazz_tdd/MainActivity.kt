c1b4f9ca7a4aabe63835b27010af953ac95b7133
Túlio <francatm@outlook.com>
2019-05-12 22:01:20 -0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/tulio/learn_fazz_tdd/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.tulio.learn_fazz_tdd
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
